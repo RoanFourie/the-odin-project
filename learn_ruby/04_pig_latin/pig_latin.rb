6b972c323f22b152079450d525ced324979f0887
Roan Fourie <roanfourie@gmail.com>
2016-12-13 19:12:32 +0200

    learn_ruby: completed 04_pig_latin

--- learn_ruby/04_pig_latin/pig_latin.rb
+++ learn_ruby/04_pig_latin/pig_latin.rb
@@ -2,14 +2,28 @@
 def translate (phrase)
   # character = ""
   vowel = %w{a o i u e}
+  list_of_results = []
   result = ""
-  (0..2).each do |i|
-    # result = (vowel.include?(phrase[i]) ? phrase : phrase + "ay")
-    if vowel.include?(phrase[0])
-      result = phrase + "ay"
+  l = phrase.length
+  (phrase.split()).each do |word|
+    if (word[0..1]).include? "qu"
+      list_of_results << word[2..l] + word[0..1] + "ay"
+    elsif vowel.include?(word[0])
+      list_of_results << word + "ay"
+    elsif !(vowel.include?(word[0])) && !(vowel.include?(word[1]))
+      if !(vowel.include?(word[2]))
+        list_of_results << word[3..l] + word[0..2] + "ay"
+      elsif (word[1..2]).include? "qu"
+        list_of_results << word[3..l] + word[0..2] + "ay"
+      else
+        list_of_results << word[2..l] + word[0..1] + "ay"
+      end
+    elsif !(vowel.include?(word[0]))
+      list_of_results << word[1..l] + word[0] + "ay"
     else
-      result = phrase
+      list_of_results << word
     end
   end
+  result = list_of_results.join(" ")
   return result
 end
