f10204db10328cbf37425e3a1c64ca9a2d1af0aa
Roan Fourie <roanfourie@gmail.com>
2016-12-13 20:40:31 +0200

    learn_ruby: comleted 05_book_titles.rb (classes, setter methods)

--- learn_ruby/05_book_titles/book.rb
+++ learn_ruby/05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
-# write your code here
+  def title=(book_title)
+    _collection = []
+    _exceptions = %w{the a an and in of}
+    (book_title.split()).each do |_words|
+      if _exceptions.include?(_words)
+        _collection << _words
+      else
+        _collection << _words.capitalize
+      end
+    end
+    _collection[0] = _collection[0].capitalize
+    @title = _collection.join(" ")
+  end
+  def title
+    @title
+  end
 end
